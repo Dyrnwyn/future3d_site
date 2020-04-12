2b662b835d0b35b83184686c2922e2e63f6b38eb
Roman Gorovoi <dyrnwyn@yandex.ru>
2020-04-12 14:15:33 +0700

    new model

---
+++ manage_product/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ProductId
+
+admin.site.register(ProductId)
+
+# Register your models here.
---
+++ manage_product/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+
+
+class ProductId(models.Model):
+	template_choise = (('1527', '1527'),
+                       ('2032', '2032'),
+                       ('2104', '2104'),
+                       ('4021', '4021'),
+                       ('4030', '4030'))
+	product_id = models.UUIDField(unique=True, primary_key=True, editable=False, 
+		default=uuid.uuid4, verbose_name="Уникальный ID изделия")
+	img = models.ImageField(upload_to='img/', verbose_name="Фото")
+	template = models.CharField(max_length = 4, choices=template_choise, verbose_name="Номер шаблона")
+	payment_id = models.CharField(max_length = 10, verbose_name="id оплаты")
+	paid = models.BooleanField(default=False, verbose_name="Оплачено")
